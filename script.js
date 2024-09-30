7d0c8072b21bb1b129d40addc5df93f0db812024
Fernanda-Bley <89091540+Fernanda-Bley@users.noreply.github.com>
2024-09-30 20:54:08 +0000

    script y nuevos database

--- script.js
+++ script.js
@@ -1,32 +1,12 @@
-// script.js
-var rows = [];
-
-// Initialize rows with countries and 0 Smoking consumption
-var countries = [
-    'Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria',
-    'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan',
-    'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cabo Verde', 'Cambodia',
-    'Cameroon', 'Canada', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo, Democratic Republic of the', 'Congo, Republic of the',
-    'Costa Rica', 'Côte d’Ivoire', 'Croatia', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic',
-    'East Timor (Timor-Leste)', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Eswatini', 'Ethiopia', 'Fiji',
-    'Finland', 'France', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Greece', 'Grenada', 'Guatemala',
-    'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran',
-    'Iraq', 'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati',
-    'Korea, North', 'Korea, South', 'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia',
-    'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta',
-    'Marshall Islands', 'Mauritania', 'Mauritius', 'Mexico', 'Micronesia, Federated States of', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Morocco',
-    'Mozambique', 'Myanmar (Burma)', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria',
-    'North Macedonia', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines',
-    'Poland', 'Portugal', 'Qatar', 'Romania', 'Russia', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines', 'Samoa',
-    'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia',
-    'Solomon Islands', 'Somalia', 'South Africa', 'Spain', 'Sri Lanka', 'Sudan', 'Sudan, South', 'Suriname', 'Sweden', 'Switzerland',
-    'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey',
-    'Turkmenistan', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Vanuatu',
-    'Vatican City', 'Venezuela', 'Vietnam', 'Yemen', 'Zambia', 'Zimbabwe'
-];
-countries.forEach(function(country) {
- rows.push({ location: country, Smoking: 0 });
-});
+console.error = function(message) {
+  // Ignore the violation if it's the Plotly warning
+  if (message.includes("plotly-2.34.0.min.js:8 [Violation]Added non-passive event listener to a scroll-blocking 'wheel' event. Consider marking event handler as 'passive' to make the page more responsive. See ")) {
+    return;
+  }
+  // Otherwise, log the error as usual
+  console.log(message);
+};
+rows = [];
 
 function unpack(rows, key) {
   return rows.map(function(row) {
@@ -34,26 +14,178 @@ function unpack(rows, key) {
   });
 }
 
-var data = [{
-  type: 'choropleth',
-  locationmode: 'country names',
-  locations: unpack(rows, 'location'),
-  z: unpack(rows, 'Smoking'),
-  text: unpack(rows, 'location'),
-  colorscale: 'diverging',
-  colorbar: {
-    title: 'Número de fumadores'
-  },
-  autocolorscale: false
-}];
+// Read the CSV file
+// d3.csv("./InvestigacionMaximos/data/Population-Smokers-2010.csv", function(error, data) {
+//   if (error) {
+//     rows.push({
+//       "population": error['Population'],
+//       "country": error['Entity']
+//     })
 
-var layout = {
-  title: 'Fumadores',
-  geo: {
-    projection: {
-      type: 'robinson'
-    }
+//     var data = [{
+//       type: 'choropleth',
+//       locationmode: 'country names',
+//       locations: unpack(rows, 'country'),
+//       z: unpack(rows, 'population'),
+//       text: unpack(rows, 'country'),
+//       hoverinfo: 'none',
+//       colorscale: [
+//     [0, '#F9E400'],
+//     [0.25, "#06D001"],
+//     [0.5, "#332FD0"],
+//     [0.75,'#AF47D2'],
+//     [1, '#F5004F']
+//     ],
+//       zmin: 10067,
+//       zmax: 2707862957      ,
+//       colorbar: {
+//         title: {
+//           text: "Fumadores"
+//         }
+//       },
+//       autocolorscale: false
+//     }];
+  
+      
+  
+//       // Create the layout
+//     var annotations = [];
+  
+//     var layout = {
+//       title: {
+//         text: 'Fumadores totales en el año 2010',
+//         x: 0.48,
+//         xanchor: 'center'
+//       },
+//       geo: {
+//         projection: {
+//         type: 'robinson'
+//         }
+//       },
+//       annotations: annotations
+//     };
+  
+//       // Call Plotly.newPlot outside of the d3.csv callback function
+//     plotMap(data, layout);
+    
+
+//     return;
+//   }
+//   });
+
+// d3.csv("./InvestigacionMaximos/data/Population-Smokers-1997.csv", function(error, data) {
+//   if (error) {
+//     rows.push({
+//       "population": error['Population'],
+//       "country": error['Entity']
+//     })
+
+//     var data = [{
+//       type: 'choropleth',
+//       locationmode: 'country names',
+//       locations: unpack(rows, 'country'),
+//       z: unpack(rows, 'population'),
+//       text: unpack(rows, 'country'),
+//       hoverinfo: 'none',
+//       colorscale: [
+//     [0, '#F9E400'],
+//     [0.25, "#06D001"],
+//     [0.5, "#332FD0"],
+//     [0.75,'#AF47D2'],
+//     [1, '#F5004F']
+//     ],
+//       zmin: 10067,
+//       zmax: 2707862957      ,
+//       colorbar: {
+//         title: {
+//           text: "Fumadores"
+//         }
+//       },
+//       autocolorscale: false
+//     }];
+  
+      
+  
+//       // Create the layout
+//     var annotations = [];
+  
+//     var layout = {
+//       title: {
+//         text: 'Fumadores totales en el año 1997',
+//         x: 0.48,
+//         xanchor: 'center'
+//       },
+//       geo: {
+//         projection: {
+//         type: 'robinson'
+//         }
+//       },
+//       annotations: annotations
+//     };
+  
+//       // Call Plotly.newPlot outside of the d3.csv callback function
+//     plotMap(data, layout);
+    
+
+//     return;
+//   }
+//   });
+
+
+d3.csv("./InvestigacionMaximos/data/Population-Smokers-1997.csv", function(error, data) {
+  if (error) {
+    rows.push({
+    "population": 0,
+    "country": error['Entity']
+    })
   }
-};
+  var data = [{
+    type: 'choropleth',
+    locationmode: 'country names',
+    locations: unpack(rows, 'country'),
+    z: unpack(rows, 'population'),
+    text: unpack(rows, 'country'),
+    hoverinfo: 'none',
+    colorscale: [
+    [0, '#F9E400'],
+    [0.25, "#06D001"],
+    [0.5, "#332FD0"],
+    [0.75,'#AF47D2'],
+    [1, '#F5004F']
+    ],
+    zmin: 10067,
+    zmax: 2707862957      ,
+    colorbar: {
+      title: {
+        text: "Fumadores"
+      }
+    },
+    autocolorscale: false
+  }];
+  
+  var annotations = [];
+      
+  var layout = {
+    title: {
+      text: 'Fumadores totales en el año XXXX',
+      x: 0.48,
+      xanchor: 'center'
+    },
+    geo: {
+      projection: {
+        type: 'robinson'
+      }
+    },
+    annotations: annotations
+  };
+      
+          // Call Plotly.newPlot outside of the d3.csv callback function
+  plotMap(data, layout);
+  return;
+});
 
-Plotly.newPlot("myDiv", data, layout, {showLink: false});
+
+// Define a function to plot the map
+function plotMap(data, layout) {
+  Plotly.newPlot("myDiv", data, layout, {showLink: false});
+}
